34e2d09119481dac4c88884fb3b0c1d56063170e
manassehpawlos-oss <manassehpawlos@gmail.com>
2026-09-18 16:07:20 -0700

    Update unicast_test.py

--- unicast_test.py
+++ unicast_test.py
@@ -2,32 +2,33 @@ from scapy.all import *
 import sys
 import os
 
+#INDICATE DEV NAME HERE!
+dev_name = "wlp0s20u2"
 
 try:
     sleep_period = int(sys.argv[1])
 except:
-    print("Using default timing value of 1s")
-    sleep_period = 1
+    print("Using default timing value of 1ms")
+    sleep_period = 1/1000
     
 TA = "AA:AA:AA:AA:AA:AA"
 SA = "BB:BB:BB:BB:BB:BB"
 seqnum=500
 
 radiotap = RadioTap(bytes.fromhex("000014000e8a000002046c09a000000000003800"))
-data_frame = Dot11(bytes.fromhex("0842000001005e0000fc82194d39745034194d39745e30ef8e9000a02e000000")
-                   + bytes.fromhex("46c759cde0ef4a2dafff57c9d73b937a35306ca7dbfd479589cf8b34e68da79eefcacc51aa9085f6b011056872c19b767e04cb8f2ac1e0fde6526163746fe5a61988f81df1804b391b4f8bfbe8b9512431f38763938d83"))
-assembled_frame = radiotap/data_frame
+data_frame = Dot11(addr1 = TA, addr2 = SA, SC=500*16, type=2)
+ccmp = Dot11CCMP(bytes.fromhex("46c759cde0ef4a2dafff57c9d73b937a35306ca7dbfd479589cf8b34e68da79eefcacc51aa9085f6b011056872c19b767e04cb8f2ac1e0fde6526163746fe5a61988f81df1804b391b4f8bfbe8b9512431f38763938d83"))
+assembled_frame = radiotap/data_frame/ccmp
 
 
 if os.getuid() == 0:
     print("Beginning scan.")
     time.sleep(1)
     while True:
-        sendp(assembled_frame, iface="wlan0mon")
-        time.sleep(sleep_period)
+        sendp(assembled_frame, iface=dev_name)
+        time.sleep(sleep_period/1000)
 else:
     print("Program not run as sudo, will open Wireshark instead")
     wireshark(assembled_frame)
-
 
 
